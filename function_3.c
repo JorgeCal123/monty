0f2e281b7fbf91d75b639928c312577555413a4c
JorgeCal123/boom.git <git@github.com>
2022-01-16 20:43:48 -0800

    funtions rotates

--- function_3.c
+++ function_3.c
@@ -70,3 +70,52 @@ void pstr(stack_t **stack, unsigned int line_number)
 
 	printf("\n");
 }
+
+
+/**
+  * rotl - Rotates the stack to the top
+  * @stack: The head of the stack
+  * @line_number: The line on which the error occurred
+  */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *curr = *stack;
+	unsigned int temp = 0;
+	(void) line_number;
+
+	if (curr && curr->next)
+	{
+		while (curr->next != NULL)
+		{
+			temp = curr->n;
+			curr->n = curr->next->n;
+			curr->next->n = temp;
+			curr = curr->next;
+		}
+	}
+}
+
+
+
+/**
+  * rotr - Rotates the stack to the bottom
+  * @stack: The head of the stack
+  * @line_number: The line on which the error occurred
+  */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *last = NULL;
+	(void) line_number;
+
+	if (*stack && (*stack)->next)
+	{
+		last = *stack;
+
+		while (last->next != NULL)
+			last = last->next;
+
+		last->prev->next = NULL;
+		last->next = *stack;
+		*stack = last;
+	}
+}
