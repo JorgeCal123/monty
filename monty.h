75ba71d98d4cec27216683d718fd50c3d3d29545
JorgeCal123/boom.git <git@github.com>
2022-01-16 18:04:42 -0800

    function sub corr 1

--- monty.h
+++ monty.h
@@ -94,7 +94,7 @@ void swap(stack_t **stack, unsigned int line_number);
 /*		function_2.c		*/
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number)
+void sub(stack_t **stack, unsigned int line_number);
 
 
 /*		list.c			 */
